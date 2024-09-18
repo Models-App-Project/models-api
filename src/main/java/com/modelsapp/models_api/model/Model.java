6bc0dff617e55a6ae053c9be696c32bb745cdb32
Breno Amaral <contato.brenoamaral.s@gmail.com>
2024-09-18 09:49:59 -0300

    fix: generate type of ID

--- src/main/java/com/modelsapp/models_api/model/Model.java
+++ src/main/java/com/modelsapp/models_api/model/Model.java
@@ -16,7 +16,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Model {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
