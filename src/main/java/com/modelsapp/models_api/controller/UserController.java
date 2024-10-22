4d8d8c21fc083276df61a2cc2b4b62a21e3f338d
Lucas Lopes <lucasslopes2004@gmail.com>
2024-10-21 22:19:42 -0300

    Adicionando a proteção contra csrf e o limite de requisições...

--- src/main/java/com/modelsapp/models_api/controller/UserController.java
+++ src/main/java/com/modelsapp/models_api/controller/UserController.java
@@ -3,41 +3,63 @@ package com.modelsapp.models_api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 import com.modelsapp.models_api.entity.User;
 import com.modelsapp.models_api.service.UserService;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+
+import java.time.Duration;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    private Bandwidth limit = Bandwidth.classic(20, Refill.greedy(20, Duration.ofMinutes(1)));
+    private final Bucket bucket = Bucket.builder()
+            .addLimit(limit)
+            .build();
+
     @Autowired
     private UserService userService;
 
     @PostMapping
     public ResponseEntity<String> salvarUsuario(@RequestBody User usuario) {
-        User usuariosalvo = userService.salvarUsuario(usuario);
-
-        return new ResponseEntity<>("Novo usuário criado " + usuariosalvo.getUsername(), HttpStatus.CREATED);
+        if (bucket.tryConsume(1)) {
+            User usuariosalvo = userService.salvarUsuario(usuario);
+            return new ResponseEntity<>("Novo usuário criado " + usuariosalvo.getUsername(), HttpStatus.CREATED);
+        }
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 
     @PutMapping
     public ResponseEntity<String> atualizarUsuario(@RequestBody User usuario) {
-        User usuariosalvo = userService.salvarUsuario(usuario);
-        return new ResponseEntity<>("Novo usuário criado " + usuariosalvo.getUsername(), HttpStatus.CREATED);
+        if (bucket.tryConsume(1)) {
+            User usuariosalvo = userService.salvarUsuario(usuario);
+            return new ResponseEntity<>("Usuário atualizado " + usuariosalvo.getUsername(), HttpStatus.CREATED);
+        }
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 
     @GetMapping
     public ResponseEntity<List<User>> obterUsuarios() {
-        return new ResponseEntity<>(userService.obterUsuarios(), HttpStatus.OK);
+        if (bucket.tryConsume(1)) {
+            return new ResponseEntity<>(userService.obterUsuarios(), HttpStatus.OK);
+        }
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 
     @DeleteMapping
-    public void excluirUsuario(@RequestBody User usuario) {
-        userService.excluirUsuario(usuario);
+    public ResponseEntity<String> excluirUsuario(@RequestBody User usuario) {
+        if (bucket.tryConsume(1)) {
+            userService.excluirUsuario(usuario);
+        }
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 
 }
