52faef578b03beb86741e6fab95abf8d0d17bf16
Breno Amaral <contato.brenoamaral.s@gmail.com>
2024-10-24 08:57:39 -0300

    latest version - broken

--- src/main/java/com/modelsapp/models_api/entity/AuthenticatedUser.java
+++ src/main/java/com/modelsapp/models_api/entity/AuthenticatedUser.java
@@ -1,10 +1,16 @@
 package com.modelsapp.models_api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticatedUser {
 
     private String username;
