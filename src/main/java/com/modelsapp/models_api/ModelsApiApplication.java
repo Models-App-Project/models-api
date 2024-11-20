24f4911ce9a9a60962923fb2aa1a6646b0f220da
Breno Amaral <contato.brenoamaral.s@gmail.com>
2024-11-19 22:39:06 -0300

    bbbbbbbbbbbbb

--- src/main/java/com/modelsapp/models_api/ModelsApiApplication.java
+++ src/main/java/com/modelsapp/models_api/ModelsApiApplication.java
@@ -1,16 +1,9 @@
 package com.modelsapp.models_api;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.modelsapp.models_api.entity.Role;
-import com.modelsapp.models_api.entity.User;
-import com.modelsapp.models_api.permission.EnumPermission;
 import com.modelsapp.models_api.service.UserService;
 
 @SpringBootApplication
